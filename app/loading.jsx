96148c9b516b7db6474997fc0dc3fc72fe6a0968
Aminul Islam <aminulds61@gmail.com>
2023-02-10 16:04:17 +0600

    loading effect added

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <Image className="h-96" width={100} height={100} src="spinner.svg" alt="loading..." />
+    </div>
+  )
+}
+
+export default Loading
